0528f4dc699018c46fb87ae6421a8ed6dc22a933
Iwona007 <iwonka.adamkiewicz@gmail.com>
2022-05-03 19:07:30 +0200

    Initial commit

---
+++ src/main/java/pl/iwona/liquibaselearning/LiquibaseLearningApplication.java
@@ -0,0 +1,13 @@
+package pl.iwona.liquibaselearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/iwona/liquibaselearning/LiquibaseLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.iwona.liquibaselearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
